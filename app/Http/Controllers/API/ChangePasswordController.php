47db745a17778eef57bca67f748e472266793c92
Allan Justine Mascariñas <90177495+allanjustine@users.noreply.github.com>
2025-05-15 15:22:47 +0800

    Updated

--- app/Http/Controllers/API/ChangePasswordController.php
+++ app/Http/Controllers/API/ChangePasswordController.php
@@ -12,30 +12,30 @@ class ChangePasswordController extends Controller
 {
 
     public function changePassword(Request $request, $id)
-{
-    // Find the user by id
-    $user = User::find($id);
-    if (!$user) {
-        return response()->json(['error' => 'User not found.'], 404);
+    {
+        // Find the user by id
+        $user = User::where("id", $id)->first();
+        if (!$user) {
+            return response()->json(['error' => 'User not found.'], 404);
+        }
+
+        // Validate request data
+        $request->validate([
+            'current_password' => 'required',
+            'new_password' => 'required|string|min:5|confirmed|different:current_password',
+        ]);
+
+        // Verify if the current password matches the user's password in the database
+        if (!Hash::check($request->current_password, $user->password)) {
+            return response()->json(['error' => 'Current password is incorrect.'], 422);
+        }
+
+        // Update the user's password
+        $user->password = Hash::make($request->new_password);
+        $user->save();
+
+        Auth::guard('web')->login($user);
+
+        return response()->json(['success' => 'Password changed successfully.'], 200);
     }
-
-    // Validate request data
-    $request->validate([
-        'current_password' => 'required',
-        'new_password' => 'required|string|min:5|confirmed',
-    ]);
-
-    // Verify if the current password matches the user's password in the database
-    if (!Hash::check($request->current_password, $user->password)) {
-        return response()->json(['error' => 'Current password is incorrect.'], 422);
-    }
-
-    // Update the user's password
-    $user->password = Hash::make($request->new_password);
-    $user->save();
-
-    Auth::guard('web')->login($user);
-
-    return response()->json(['success' => 'Password changed successfully.'], 200);
-}
 }
