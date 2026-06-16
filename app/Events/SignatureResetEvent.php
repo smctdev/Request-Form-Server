8261b71d292c5223b388b5c2c1b602d0bbf8941a
Allan Justine Mascariñas <90177495+allanjustine@users.noreply.github.com>
2026-06-16 13:50:43 +0800

    added admin can reset signature and password

---
+++ app/Events/SignatureResetEvent.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\User;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class SignatureResetEvent implements ShouldBroadcast, ShouldQueue
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(public User $user)
+    {
+        //
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new PrivateChannel("signature.reset.{$this->user->id}"),
+        ];
+    }
+
+    public function broadcastAs()
+    {
+        return 'signature-reset';
+    }
+
+    public function broadcastWith()
+    {
+        return [
+            'message' => 'Your signature has been reset by admin. You can now draw your signature again.',
+        ];
+    }
+}
