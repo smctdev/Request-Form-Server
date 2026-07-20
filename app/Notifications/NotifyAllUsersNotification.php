e12bd84e9c3c51f4fcdddd8bd450c667b27f8a2f
Allan Justine Mascariñas <90177495+allanjustine@users.noreply.github.com>
2026-07-20 14:45:31 +0800

    notification for all users re,ove should broadcast

--- app/Notifications/NotifyAllUsersNotification.php
+++ app/Notifications/NotifyAllUsersNotification.php
@@ -3,13 +3,12 @@
 namespace App\Notifications;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\BroadcastMessage;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class NotifyAllUsersNotification extends Notification implements ShouldQueue, ShouldBroadcast
+class NotifyAllUsersNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
